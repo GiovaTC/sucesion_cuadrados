c155d7b5f85dc02b531e886033591de0e72aa4fd
gtapiero <giovanny.tapiero@pi.edu.co>
2025-08-26 11:02:00 -0500

    Agregar archivos de proyecto.

---
+++ SucesionCuadrados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SucesionCuadrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // definimos cuantos terminos mostrar
+            int cantidad = 10;
+
+            Console.WriteLine("sucesion de cuadrados:");
+
+            for (int i = 1; i <= cantidad; i++)
+            {
+                int cuadrado = i * i;
+                Console.Write(cuadrado + " ");
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();        
+        }
+    }
+}
